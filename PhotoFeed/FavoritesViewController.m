f3e4bf07e544b2b6d996ca9a1be21aa7f677a2b6
Kylie Le <kylie.tram.le@gmail.com>
2016-11-30 13:54:36 -0800

    button inside favorites controller and leads to image view controller that displays button image in full size

--- PhotoFeed/FavoritesViewController.m
+++ PhotoFeed/FavoritesViewController.m
@@ -28,6 +28,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
+    UIButton *pigView = [UIButton buttonWithType:UIButtonTypeCustom];
+    [pigView setImage:[UIImage imageNamed:@"pig.png"] forState:UIControlStateNormal];
+    [pigView setImage:[UIImage imageNamed:@"pig.png"] forState:UIControlStateHighlighted]; // because button is custom
+    pigView.frame = CGRectMake(60, 100, 240, 240);
+    [self.view addSubview:pigView];
+    [pigView addTarget:self action:@selector(showZoomedPicture:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+-(void)showZoomedPicture:(UIButton *)sender
+{
+    UIViewController *pigImageViewController = [[UIViewController alloc] init]; // VC on the fly, not subclass
+    pigImageViewController.view.frame = self.view.frame;
+    pigImageViewController.title = @"Pig Button"; // title shows on top of the controller
+    
+    UIImageView *pigView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pig.png"]];
+    [pigView setContentMode:UIViewContentModeScaleAspectFit];
+    pigView.frame = pigImageViewController.view.frame;
+    [pigImageViewController.view addSubview:pigView]; // add image subview into new controller
+    
+    [self.navigationController pushViewController:pigImageViewController animated:YES]; // push onto stack new image view controller
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
