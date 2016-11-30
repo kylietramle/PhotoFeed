0e1650bed88b0bf5845e5ade1c84ac6eda058883
Kylie Le <kylie.tram.le@gmail.com>
2016-11-30 12:43:41 -0800

    tab bar buttons + scrollview

--- PhotoFeed/ProfileViewController.h
+++ PhotoFeed/ProfileViewController.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ProfileViewController : UIViewController
-
+@property (strong, nonatomic) UIScrollView *scrollView;
 @end
--- PhotoFeed/ProfileViewController.m
+++ PhotoFeed/ProfileViewController.m
@@ -30,10 +30,38 @@
     
     self.view.backgroundColor = [UIColor yellowColor];
     
-    // add image view
+    //scroll view
+    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+//    self.scrollView.contentSize = CGSizeMake(320, 480); // 480 = 440 + 40 from memberSinceLabel
+    self.scrollView.autoresizingMask = (UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth); // resize in landscape too
+    [self.view addSubview:self.scrollView];
+    
+    // image view
     UIImageView *willyView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"willy.jpg"]];
-    willyView.frame = CGRectMake(20, 20, 100, 114);
-    [self.view addSubview:willyView];
+    willyView.frame = CGRectMake(20, 20, 110, 114);
+    [self.scrollView addSubview:willyView];
+    
+    // name label
+    UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 140, 280, 40)];
+    nameLabel.text = @"Name: Willy";
+    [self.scrollView addSubview:nameLabel];
+    
+    //city label
+    UILabel *cityLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 200, 280, 40)];
+    cityLabel.text = @"From: Korea";
+    [self.scrollView addSubview:cityLabel];
+    
+    // biography
+    UITextView *biography = [[UITextView alloc] initWithFrame:CGRectMake(12, 260, 300, 180)];
+    biography.font = [UIFont fontWithName:@"Helvetica" size:15];
+    biography.editable = NO;
+    biography.text = @"Willy is the king of all Williers. Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray! Hooray!";
+    [self.scrollView addSubview:biography];
+    
+    // member since
+    UILabel *memberSinceLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 440, 280, 40)];
+    memberSinceLabel.text = @"Member since: September 1989";
+    [self.scrollView addSubview:memberSinceLabel];
     
 }
 
